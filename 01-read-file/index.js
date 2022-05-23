9a27d1919bcbc3cb755eec77213916d5debea98d
hannarim-23 <80169404+hannarim-23@users.noreply.github.com>
2022-05-23 18:56:55 +0300

    feat: task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path= require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+const readableStream=fs.createReadStream(filePath,'utf-8');
+
+readableStream.on('data', chunk=>{
+    console.log(chunk);
+});
+
+readableStream.on('error', error => console.log('Error', error.message));
